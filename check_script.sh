2d73011bcb82ad895fec9c861cb0a4350c8f9c37
liuhuijun <liuhuijun@debian6.(none)>
2015-01-12 13:56:36 -0500

    add check script for document

---
+++ check_script.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#
+# add extern git command
+#
+do_extern_git() {
+	# about zjhn	
+	git checkout zjhn
+	git pull origin zjhn
+	
+	# about master
+	# git checkout master
+	# git pull
+        
+	#git branch -v
+}
+
+do_normal_git() {
+	git status .
+}
+
+#
+# print line of '\='
+#
+print_line() {
+	echo "====================$*======================"
+}
+
+main() {
+	local file=list.in
+	local tmp=${file}.tmp
+	local docs
+
+	sed '/#/d;/^$/d' ${file}> ${tmp}
+	echo "--document list--"
+	cat ${tmp}
+	
+	while read docs; do
+		cd ${docs}
+		print_line enter ${docs}
+		do_normal_git
+		do_extern_git
+		print_line leave ${docs}
+		cd - > /dev/null
+		sleep 2
+	done < ${tmp}
+	rm ${tmp}
+}
+main "$@"
