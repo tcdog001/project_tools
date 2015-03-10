a48a23db71a5fa5ed4d9f2a20d3f3033e1f752d5
liuhj <liuhuijun@debian.suns>
2015-03-10 06:06:02 -0400

    modify check_script.sh

--- check_script.sh
+++ check_script.sh
@@ -2,19 +2,29 @@
 
 #
 # add extern git command
+# $1: doc
 #
 do_extern_git() {
+	local doc=$1
 	git branch --set-upstream  master  origin/master
 	git branch --set-upstream  zjhn  origin/zjhn
 	
+	if [ "${doc}" != "website" ]; then
 	# about zjhn	
 	git checkout zjhn
 	
 	# about master
 	#git checkout master
+	else
+		# when doc is website
+		git checkout master
+        fi
         
 	#git branch -v
 	git pull
+	#git push
+	#git tag -a v1.0.6.8 -m "version 1.0.6.8: about User Authentication for zjhn"
+	#git push --tag
 }
 
 do_normal_git() {
@@ -41,7 +51,7 @@ main() {
 		cd ${docs}
 		print_line enter ${docs}
 		do_normal_git
-		do_extern_git
+		do_extern_git ${docs}
 		print_line leave ${docs}
 		cd - > /dev/null
 		sleep 2
